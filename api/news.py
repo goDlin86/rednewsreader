43b0fc24141b7f9fb15bfd9aa5e5ec5c1ea4f789
goDlin <godlin086@gmail.com>
2021-01-21 19:51:56 +0300

    with news

--- api/news.py
+++ api/news.py
@@ -7,6 +7,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_POST(self):
         themes = ['world', 'nation', 'scitech', 'entertainment', 'sports', 'business']
+        news = []
 
         # content_len = int(self.headers['content-length'])
         # post_body = self.rfile.read(content_len)
@@ -14,26 +15,26 @@ class handler(BaseHTTPRequestHandler):
         # theme = data['theme']
         theme = 'world'
 
-        # if theme in themes:
-        #     url = 'https://news.google.com/news/rss/headlines/section/topic/'+theme.upper()+'.ru_ru/?ned=ru_ru&hl=ru'
-        #     r = requests.get(url)
-        #     xml = etree.parse(r.text)
-        #     #r.close()
-        #     root = xml.getroot()
+        if theme in themes:
+            url = 'https://news.google.com/news/rss/headlines/section/topic/'+theme.upper()+'.ru_ru/?ned=ru_ru&hl=ru'
+            r = requests.get(url)
+            xml = etree.parse(r.text)
+            #r.close()
+            root = xml.getroot()
 
-        #     for item in root.find('channel').iterfind('item'):
-        #         news.append({
-        #             'title': item.find('title').text,
-        #             'link': item.find('link').text,
-        #             'description': item.find('description').text,
-        #             'date': item.find('pubDate').text
-        #         })
-        # else:
-        #     news.append({'title': 'Not found theme'})
+            for item in root.find('channel').iterfind('item'):
+                news.append({
+                    'title': item.find('title').text,
+                    'link': item.find('link').text,
+                    'description': item.find('description').text,
+                    'date': item.find('pubDate').text
+                })
+        else:
+            news.append({'title': 'Not found theme'})
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(json.dumps({'title': 'asdads'}).encode())
-        # self.wfile.write(json.dumps(news).encode())
+        # self.wfile.write(json.dumps({'title': 'asdads'}).encode())
+        self.wfile.write(json.dumps(news).encode())
         return
