ac777eb0238ae36a6103b0d3df1a04169b9800ef
goDlin <godlin086@gmail.com>
2021-04-14 17:50:42 +0300

    ssr update

--- pages_lib/NewsList.js
+++ pages_lib/NewsList.js
@@ -19,13 +19,13 @@ ReactGA.initialize('UA-26528518-5')
 
 const fetcher = url => fetch(url).then(res => res.json())
 
-export default function Main() {
-    const router = useRouter()
-    const t = themes.find(t => t.link === '/' + router.query.theme) || themes[0]
+export default function NewsList({ theme, initialData }) {
+    const { query } = useRouter()
+    const t = themes.find(t => t.link === '/' + query.theme) || themes[0]
     const { data, error } = useSwr(
-        router.query.theme ? '/api/' + router.query.theme : null, 
+        query.theme ? '/api/' + query.theme : null, 
         fetcher,
-        { revalidateOnFocus: false }
+        { initialData, revalidateOnFocus: false }
     )
 
     const trail = useTrail(8, {
@@ -46,7 +46,7 @@ export default function Main() {
             time: dayjs(item.date).fromNow()
         }
     ))
-    ReactGA.pageview(router.query.theme)
+    ReactGA.pageview(query.theme)
         
     return (
         <>
@@ -59,4 +59,9 @@ export default function Main() {
             {items.slice(8).map((item, i) => <Item item={item} color={t.color} key={10+i} />)}
         </>
     )
+}
+
+export async function getServerSideProps({ query }) {
+    const data = await fetcher('/api/' + query.theme)
+    return { props: { initialData: data, theme: query.theme } }
 }
