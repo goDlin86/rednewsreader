fb996677844fef1c5448ff03a90d2654812fdbb8
goDlin <godlin086@gmail.com>
2021-04-29 20:54:34 +0300

    ssr

--- pages/[theme].js
+++ pages/[theme].js
@@ -2,11 +2,19 @@ import Layout from '../pages_lib/Layout'
 import Nav from '../pages_lib/Nav'
 import NewsList from '../pages_lib/NewsList'
 
-export default function Main() {
+const fetcher = url => fetch(url).then(res => res.json())
+
+export default function Main(props) {
     return (
         <Layout>
             <Nav />
-            <NewsList />
+            <NewsList {...props} />
         </Layout>
     )
+}
+
+export async function getServerSideProps({ query }) {
+    const baseUrl = 'https://rednewsreader.vercel.app'//'http://localhost:3000'
+    const data = await fetcher(baseUrl + '/api/' + query.theme)
+    return { props: { theme: query.theme, initialData: data } }
 }
--- pages_lib/NewsList.js
+++ pages_lib/NewsList.js
@@ -1,5 +1,4 @@
 import useSwr from 'swr'
-import { useRouter } from 'next/router'
 import styles from '../styles/Home.module.css'
 
 import themes from './themes'
@@ -19,15 +18,12 @@ ReactGA.initialize('UA-26528518-5')
 const fetcher = url => fetch(url).then(res => res.json())
 
 export default function NewsList({ theme, initialData }) {
-    const { query } = useRouter()
-    const t = themes.find(t => t.link === '/' + query.theme) || themes[0]
+    const t = themes.find(t => t.link === '/' + theme) || themes[0]
     const { data, error } = useSwr(
-        query.theme ? '/api/' + query.theme : null, 
+        theme ? '/api/' + theme : null, 
         fetcher,
         { initialData, revalidateOnFocus: false }
     )
-
-    //console.log('render')
     
     if (error) return <div className={styles.footer}>Ошибка</div>
     
@@ -41,16 +37,11 @@ export default function NewsList({ theme, initialData }) {
             time: dayjs(item.date).fromNow()
         }
     ))
-    ReactGA.pageview(query.theme)
+    ReactGA.pageview(theme)
         
     return (
         <>
             {items.map((item, i) => <Item item={item} color={t.color} key={i} />)}
         </>
     )
-}
-
-export async function getServerSideProps({ query }) {
-    const data = await fetcher('/api/' + query.theme)
-    return { props: { initialData: data, theme: query.theme } }
 }
