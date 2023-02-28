c7a02e959478800ac9154c8dc8033d36e1278649
Denis <godlin086@gmail.com>
2023-02-28 20:50:24 +0300

    add stagger children

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,22 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 import styles from '../styles/Home.module.css'
 
 export default function Loading() {
     return (
-        <div className={styles.footer}>Загрузка...</div>
+        <motion.div
+            initial={{ opacity: 0, y: 0 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 25 }}
+            transition={{
+                type: "spring",
+                stiffness: 140,
+                damping: 20
+            }}
+        >
+            <div className={styles.footer}>Загрузка...</div>
+        </motion.div>
     )
 }
--- pages_lib/NewsList.js
+++ pages_lib/NewsList.js
@@ -5,33 +5,35 @@ import { motion, AnimatePresence } from 'framer-motion'
 import Item from './Item'
 
 const upContent = {
-    initial: { y: 120, opacity: 0 },
-    animate: { y: 0, opacity: 1, transition: { delay: 0.4 } },
-    exit: { y: 50, opacity: 0 },
+    initial: { y: 100, opacity: 0, transition: { type: 'spring', bounce: 0, duration: 0.4, staggerChildren: 0.1, delayChildren: 0.3 } },
+    animate: { y: 0, opacity: 1, transition: { duration: 0.4, staggerChildren: 0.1 } },
+    //exit: { y: 50, opacity: 0 },
+}
+const itemVariants = {
+    initial: { y: 20, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
 }
 
 export default function NewsList({ items, color }) {
 
         
     return (
-        <AnimatePresence initial={false} mode='wait' onExitComplete={() => window.scrollTo(0, 0)}>
+        <>
+        {/* <AnimatePresence initial={false} mode='wait' onExitComplete={() => window.scrollTo(0, 0)}> */}
             <motion.div
                 key={color}
-                // initial='initial'
-                // animate='animate'
-                // exit='exit'
-                // variants={upContent}
-                initial={{ opacity: 0, y: 25 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 25 }}
-                transition={{
-                    type: "spring",
-                    stiffness: 140,
-                    damping: 20
-                }}
+                initial='initial'
+                animate='animate'
+                //exit='exit'
+                variants={upContent}
             >
-                {items.map((item, i) => <Item item={item} color={color} key={i} />)}
+                {items.map((item, i) => (
+                    <motion.div variants={itemVariants} key={i}>
+                        <Item item={item} color={color} />
+                    </motion.div>
+                ))}
             </motion.div>
-        </AnimatePresence>
+        {/* </AnimatePresence> */}
+        </>
     )
 }
