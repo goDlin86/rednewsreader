c4fd4eff3bea4ab76ac606bd3018295dccbd8fa4
Denis <godlin086@gmail.com>
2023-03-04 08:43:29 +0300

    fix function

--- pages/api/inngest.js
+++ pages/api/inngest.js
@@ -3,12 +3,12 @@ import { serve } from 'inngest/next'
 
 export const inngest = new Inngest({ name: 'rednewsreader' })
 
-const themes = ['world', 'nation', 'sports', 'scitech', 'entertainment', 'business']
-
 const revalidate = inngest.createFunction(
   { name: 'Revalidate RedNewsReader' }, 
   { cron: '0 */2 * * *' }, 
   async ({ event, step }) => {
+    const themes = ['world', 'nation', 'sports', 'scitech', 'entertainment', 'business']
+    
     for (const theme in themes) {
       await step.run(theme, async () => await f(theme))
     }
